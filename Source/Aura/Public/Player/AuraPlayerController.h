0dd775b23df307fd19b87b38f9e3b1ec35b6b58d
cakehandsfrancis-gamedev <cakehandsfrancis@gmail.com>
2026-06-29 20:14:54 -0400

    Created Enhanced Inputs & Aura Player Controller

---
+++ Source/Aura/Public/Player/AuraPlayerController.h
@@ -0,0 +1,17 @@
+// Assets Copyright Druid Mechanics. Project Training worked on by Rashawn Francis
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "AuraPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API AAuraPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
